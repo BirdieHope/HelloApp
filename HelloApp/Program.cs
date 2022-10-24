22051ccdecefce656cdfe9889ca4b04c8e292345
BirdieHope <Bluebirdie.inthesky@gmail.com>
2022-10-24 16:51:08 +0600

    Excess text was removed

--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -10,25 +10,3 @@ tom.name = "Tom";
 //tom.age = 37;
 tom.Print();
 Console.ReadLine();
-
-/*
-Console.WriteLine("Hello, World!");
-
-string? name = " ";       // вводим имя
-Console.Write("Введите имя ");
-name = Console.ReadLine();   
-Console.WriteLine($"Привет {name}");    
-Console.ReadLine();
-
-class Person
-{
-    public string name = "Undefinite";
-    public int age;
-
-    public void Print()
-    {
-        Console.WriteLine("Имя: {0}. Возраст: {1}", name, age);
-    }
-
-}
-*/
