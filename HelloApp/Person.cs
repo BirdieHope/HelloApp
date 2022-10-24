d90850835b1ec04705d06462cf01712d750168dd
Admin <Admin@LAPTOP-ULUQEAP9>
2022-10-24 14:29:38 +0600

    Добавьте файлы проекта.

---
+++ HelloApp/Person.cs
@@ -0,0 +1,20 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+    */
+
+namespace HelloApp
+{
+    public class Person
+    {
+        public string name = "Undefinite";
+        ///public int age;
+
+        public void Print()
+        {
+            Console.WriteLine("Имя: {0}.", name);
+        }
+    }
+}
---
+++ HelloApp/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using HelloApp;
+
+Person tom = new Person();
+//string personName=tom.name;
+//int personAge=tom.age;
+//Console.WriteLine("Имя: {0}. Возраст: {1}", personName, personAge);
+tom.name = "Tom";
+//tom.age = 37;
+tom.Print();
+Console.ReadLine();
+
+/*
+Console.WriteLine("Hello, World!");
+
+string? name = " ";       // вводим имя
+Console.Write("Введите имя ");
+name = Console.ReadLine();   
+Console.WriteLine($"Привет {name}");    
+Console.ReadLine();
+
+class Person
+{
+    public string name = "Undefinite";
+    public int age;
+
+    public void Print()
+    {
+        Console.WriteLine("Имя: {0}. Возраст: {1}", name, age);
+    }
+
+}
+*/
